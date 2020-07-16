f27280400ca97c7809e489b9b26723c0939f4c32
suntod <67999195+suntod@users.noreply.github.com>
2020-07-16 15:26:18 +0700

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,49 @@ var movies = [
 	  "postDate": "2020-07-15",
 	  "effectiveDate": "2020-07-16",
 	  "unit": "Baht/Litre",
-	  "publishedBy": "BCP/MPD"
+	  "publishedBy": "BCP/MPD",
+	  "oilPrice": [
+		{
+		  "id": "hipremiumdiesel-s",
+		  "updatedPrice": 26.66,
+		  "variance": -0.30
+		},
+		{
+		  "id": "hidiesel-s",
+		  "updatedPrice": 22.19,
+		  "variance": -0.30
+		},
+		{
+		  "id": "hidiesel-s-b10",
+		  "updatedPrice": 19.19,
+		  "variance": -0.30
+		},
+		{
+		  "id": "hidiesel-b20s",
+		  "updatedPrice": 18.94,
+		  "variance": -0.30
+		},
+		{
+		  "id": "gasohol-e85s",
+		  "updatedPrice": 17.99,
+		  "variance": -0.15
+		},
+		{
+		  "id": "gasohol-e20s",
+		  "updatedPrice": 20.04,
+		  "variance": -0.30
+		},
+		{
+		  "id": "gasohol-91s",
+		  "updatedPrice": 21.28,
+		  "variance": -0.30
+		},
+		{
+		  "id": "gasohol-95s",
+		  "updatedPrice": 21.55,
+		  "variance": -0.30
+		},
+	  ]
 	}
 ];
 
