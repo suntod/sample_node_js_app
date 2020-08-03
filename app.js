6c0a74a5b29dc2511ea68eb2876fe25eee91f2b4
suntod <67999195+suntod@users.noreply.github.com>
2020-08-03 13:08:52 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,5 @@
 const express = require('express')
-//const bodyParser = require('body-parser')
-//const request = require('request')
 const app = express()
 const port = process.env.PORT || 4000
-
 app.post('/webhook', (req, res) => res.sendStatus(200))
 app.listen(port)
