cd1da092d86fbeac23d6ed4e3e5329a468b24f95
suntod <67999195+suntod@users.noreply.github.com>
2020-08-03 13:01:56 +0700

    Update app.js

--- app.js
+++ app.js
@@ -3,5 +3,11 @@ const bodyParser = require('body-parser')
 const request = require('request')
 const app = express()
 const port = process.env.PORT || 4000
-app.post('/webhook', (req, res) => res.sendStatus(200))
-app.listen(port)
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+app.post('/webhook', (req, res) => {
+    let reply_token = req.body.events[0].replyToken
+    reply(reply_token)
+    res.sendStatus(200)
+})
