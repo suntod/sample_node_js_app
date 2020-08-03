8902dfb9417da1e45f032bde19e64a682703a3cd
suntod <67999195+suntod@users.noreply.github.com>
2020-08-03 11:41:57 +0700

    Update index.js

--- index.js
+++ index.js
@@ -122,7 +122,7 @@ app.post('/api/bcpoilprice', (req, res) => {
 });
 
 app.post('/api/testsendsms', (req, res) => {
-    res.send('<h1>Hello SMS</h1>');
+    //res.send('<h1>Hello SMS</h1>');
 	res.send('Current Date is ' + Date());
 	res.send(req.body.firstName);
 });
